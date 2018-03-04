35b8cd3412a3f09a2090306b12d175d2b73e06ad
martin-91 <martin.nielsen60@gmail.com>
2018-03-04 11:25:28 +0100

    Started implementing the mirrror app specific code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'font-awesome/css/font-awesome.min.css';
+
+function TimeComponent() {
+  return (
+    <div >
+      <header>
+        <h1>{"Time: " + new Date()}</h1>
+      </header>
+    </div>
+  )
+}
+
+function WeatherComponent() {
+  return (
+    <div >
+      <h3> Looking cloudy today </h3>
+      <i className="fa fa-cloud"></i>
+      
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
@@ -8,14 +29,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to the Smart Mirror</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TimeComponent />
+        <WeatherComponent />
       </div>
     );
   }
 }
-
 export default App;
