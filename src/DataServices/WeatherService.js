6235ce36c2ebe653e3242bcd681d86e5a085656d
martin91 <martin.nielsen60@gmail.com>
2018-03-10 00:13:59 +0100

    Started mapping of weather forecasts from api

--- src/DataServices/WeatherService.js
+++ src/DataServices/WeatherService.js
@@ -1,11 +1,47 @@
+export class Forecast {
+  constructor() {
+    let forecastMoments = [];
+  };
+
+  addForecastMoment(forecastMoment) {
+    this.forecastMoments.push(forecastMoment);
+  };
+}
+
+export class ForecastMoment {
+  constructor(
+    time,
+    temp_high,
+    temp_low,
+    direction,
+    strength) {
+  }
+}
+
+function kelvinToCelsius(kelvin) {
+  let celsius = kelvin - 273.15;
+  return celsius;
+}
+
 function getCurrentWeather() {
-  var xhr = new XMLHttpRequest();
+  let xhr = new XMLHttpRequest();
   xhr.open("GET", "http://api.openweathermap.org/data/2.5/forecast?id=2694762&APPID=d5522d73c5c76fd651027ccd9f7ee924", false);
   xhr.send();
   var response = JSON.parse(xhr.responseText);
-  console.log(response);
+  //Handle the response
+  let forecast = new Forecast();
+  for (let element of response.list) {
+    let humidity = element.main.humidity
+    let forecastMoment = new ForecastMoment(
+      element.main.dt_txt,
+      kelvinToCelsius(element.main.temp_max),
+      kelvinToCelsius(element.main.temp_min),
+      element.main.humidity
+    );
+    forecast.addForecastMoment(forecastMoment)
+  }
 }
-
+// exports the above method
 export default getCurrentWeather;
 
 //Latitude: 58.402128 | Longitude: 15.60235
